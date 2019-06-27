dcfa2d3f160dbaf8addf0727984d458cb5cab44b
springercass <casprin2@gmail.com>
2019-06-27 13:10:24 -0500

    food updated

--- potluck/src/components/AddFoodForm.js
+++ potluck/src/components/AddFoodForm.js
@@ -4,70 +4,76 @@ import { connect } from "react-redux";
 import jsonwebtoken from "jsonwebtoken";
 
 class AddFood extends React.Component {
-	constructor() {
-		super();
-		this.state = {
-			user: jsonwebtoken.decode(localStorage.getItem("token"))
-				? jsonwebtoken.decode(localStorage.getItem("token"))
-				: null,
-			recipe_name: "",
-			quantity: 0,
-			guest_name: null,
-			being_brought: false
-		};
-	}
-	handleChange = event => {
-		this.setState({ ...this.state, [event.target.name]: event.target.value });
-	};
+  constructor() {
+    super();
+    this.state = {
+      user: jsonwebtoken.decode(localStorage.getItem("token"))
+        ? jsonwebtoken.decode(localStorage.getItem("token"))
+        : null,
+      recipe_name: "",
+      quantity: 0,
+      guest_name: null,
+      being_brought: false
+    };
+  }
+  handleChange = event => {
+    this.setState({ ...this.state, [event.target.name]: event.target.value });
+  };
 
-	render() {
-		console.log(this.props);
-		return (
-			<form
-				onSubmit={e => {
-					e.preventDefault();
-					this.props.addFood(
-						`http://localhost:5000/event/${this.props.event_id}/foodlist`,
-						// `https://potlucker-planner.herokuapp.com/event/${
-						// 	this.props.event_id
-						// }/foodlist`,
-						{
-							recipe_name: this.state.recipe_name,
-							quantity: parseInt(this.state.quantity),
-							guest_name: null,
-							being_brought: false
-						}
-					);
-					this.props.history.push(`/event/${this.props.event_id}`);
-				}}
-			>
-				<input
-					onChange={this.handleChange}
-					name="recipe_name"
-					id="recipe_name"
-					placeholder="Enter Food Name"
-					value={this.state.recipe_name}
-					type="text"
-					className="addElement"
-					required
-				/>
-				<input
-					onChange={this.handleChange}
-					name="quantity"
-					id="quantity"
-					placeholder="Enter Quantity"
-					value={this.state.quantity}
-					type="number"
-					className="addElement"
-					required
-				/>
-				<button> Add Food</button>
-			</form>
-		);
-	}
+  render() {
+    console.log(this.props);
+    return (
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          this.props
+            .addFood(
+              `http://localhost:5000/event/${this.props.event_id}/foodlist`,
+              // `https://potlucker-planner.herokuapp.com/event/${
+              // 	this.props.event_id
+              // }/foodlist`,
+              {
+                recipe_name: this.state.recipe_name,
+                quantity: parseInt(this.state.quantity),
+                guest_name: null,
+                being_brought: false
+              }
+            )
+            .then(() =>
+              this.props.getSingleEvent(
+                `http://localhost:5000/event/${this.props.event_id}`
+                // `https://potlucker-planner.herokuapp.com/event/${this.props.event_id}`
+              )
+            );
+        }}
+      >
+        <input
+          onChange={this.handleChange}
+          name="recipe_name"
+          id="recipe_name"
+          placeholder="Enter Food Name"
+          value={this.state.recipe_name}
+          type="text"
+          className="addElement"
+          required
+        />
+        <input
+          onChange={this.handleChange}
+          name="quantity"
+          id="quantity"
+          placeholder="Enter Quantity"
+          value={this.state.quantity}
+          type="number"
+          className="addElement"
+          required
+        />
+        <button> Add Food</button>
+      </form>
+    );
+  }
 }
 
 export default connect(
-	null,
-	{ addFood }
+  null,
+  { addFood }
 )(AddFood);
